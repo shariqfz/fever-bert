6e8b6e8a5b8318e35c4d714462894982128ff742
shariqfz <shariq.faraz97@gmail.com>
2024-04-01 04:03:40 +0530

    fixed errors

--- utils/unshared_model.py
+++ utils/unshared_model.py
@@ -16,12 +16,12 @@ class UnsharedModel(nn.Module):
             nn.Linear(256, 3)
         )
 
-    def forward(self, input_ids1, input_ids2, attention_mask1, attention_mask2, lables):
+    def forward(self, input_ids1, input_ids2, attention_mask1, attention_mask2, labels):
         output1 = self.encoder1(input_ids1, attention_mask=attention_mask1)[1]
         output2 = self.encoder2(input_ids2, attention_mask=attention_mask2)[1]
         output = torch.cat((output1, output2), dim=1)
         loss = loss = nn.CrossEntropyLoss()(outputs, labels)
-        return self.classifier(output), loss
+        return loss, self.classifier(output)
 
 # Load the BERT tokenizer
 # tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
