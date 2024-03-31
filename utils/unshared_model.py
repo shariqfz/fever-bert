1f21eb3126ab60d5a399d37b1e173b52960833a0
shariqfz <shariq.faraz97@gmail.com>
2024-04-01 04:29:40 +0530

    fixed errors

--- utils/unshared_model.py
+++ utils/unshared_model.py
@@ -6,19 +6,19 @@ from torch.utils.data import DataLoader, RandomSampler, SequentialSampler, Tenso
 from transformers import AdamW
 
 class UnsharedModel(nn.Module):
-    def __init__(self, model_name_or_path, from_tf, config, cache_dir):
+    def __init__(self, model_name_or_path, from_tf, cache_dir):
         super(UnsharedModel, self).__init__()
-        self.encoder1 = BertModel.from_pretrained(model_name_or_path, from_tf=from_tf, config=config, cache_dir=cache_dir)
-        self.encoder2 = BertModel.from_pretrained(model_name_or_path, from_tf=from_tf, config=config, cache_dir=cache_dir)
+        self.encoder1 = BertModel.from_pretrained(model_name_or_path, from_tf=from_tf, cache_dir=cache_dir)
+        self.encoder2 = BertModel.from_pretrained(model_name_or_path, from_tf=from_tf, cache_dir=cache_dir)
         self.classifier = nn.Sequential(
             nn.Linear(768 * 2, 256),
             nn.ReLU(),
             nn.Linear(256, 3)
         )
 
-    def forward(self, input_ids1, input_ids2, attention_mask1, attention_mask2, labels):
-        output1 = self.encoder1(input_ids1, attention_mask=attention_mask1)[1]
-        output2 = self.encoder2(input_ids2, attention_mask=attention_mask2)[1]
+    def forward(self, input_ids1, input_ids2, attention_mask1, attention_mask2, token_type_ids1, token_type_ids2, labels):
+        output1 = self.encoder1(input_ids=input_ids1, token_type_ids=token_type_ids1, attention_mask=attention_mask1)[1]
+        output2 = self.encoder2(input_ids=input_ids2, token_type_ids=token_type_ids2, attention_mask=attention_mask2)[1]
         output = torch.cat((output1, output2), dim=1)
         loss = loss = nn.CrossEntropyLoss()(outputs, labels)
         return loss, self.classifier(output)
